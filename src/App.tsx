d27075675d65f1ad709726667521e911718a0371
harshitsharma01 <harshit.sharma01@nagarro.com>
2024-07-31 22:12:24 +0530

    bootstrapping react application with typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold"> Welcome to the CheckList Todo App</h1>
+    </>
+  );
+}
+
+export default App;
